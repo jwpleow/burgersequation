b63c8f7d11ee14cb7ddf576956d87872362be80b
jwl16 <woeltic@gmail.com>
2019-03-02 20:03:03 +0000

    Finished implementation

--- Burgers.h
+++ Burgers.h
@@ -13,9 +13,9 @@
 #include <cmath>
 #include <iomanip>
 #include <fstream>
-#include <climits>
 
 
+// Define BLAS functions required
 #define F77NAME(x) x##_
 extern "C" {
 
@@ -26,7 +26,6 @@ double F77NAME(ddot)(const int& n, const double* dx, const int& incx, const doub
 
 
 class Burgers {
-
 public:
     // * * * * * * * * * * * * * * CONSTRUCTOR * * * * * * * * * * * * * * * * //
 
@@ -38,48 +37,32 @@ public:
 
     // * * * * * * * * * * * * * * MEMBER FUNCTIONS  * * * * * * * * * * * * * //
 
-    // Calculates the initial velocity field from the input parameters for the initial condition:
-    // At t=0; r=sqrt(x^2+y^2); u & v = 2 * (1 - r) ^4 * (4 * r + 1) for r <= 1 and = 0 for r > 1
-    void SetVelField(Model& A);
 
-    // Functions to display the u and v velocity field in the output, with the top left corner
+    void SetVelField(Model& A); ///< Calculates the initial velocity field from the input parameters
+
+    // Functions to display the u and v velocity field, with the top left corner
     // being u(-L/2,-L/2), and the bottom right corner u(L/2,L/2)
     void DisplayuVelField(Model &A);
     void DisplayvVelField(Model &A);
 
-    void DisplayCombinedufield(Model &A);
+    void TimeIntegrateVelField(Model &A); ///< Function that time integrates the velocity fields according to Burgers'
 
-    void TimeIntegrateVelField(Model &A);
+    double EnergyOfVelField(Model &A); ///< Function that returns the total energy of the velocity fields
 
-    double EnergyOfVelField(Model &A);
-
-    // Function to print the velocity fields to a file
-    void FilePrintVelFields(Model &A);
+    void FilePrintVelFields(Model &A); ///< Function to print the velocity fields to a file "VelocityFields.txt"
 
 
-    int xsize() const { return xsize_; } ///< Return x Size
-    int ysize() const { return ysize_; } ///< Return y Size
     double* udata() const { return udata_; } ///< Returns Pointer to u data
     double* vdata() const { return vdata_; }; ///< Returns Pointer to v data
+
 private:
 
-    int xsize_; // long just in case of overflow from large size of matrix required
-    int ysize_; // long just in case of overflow from large size of matrix required
     double* udata_;
     double* vdata_;
     double* udata_2;
     double* vdata_2;
     double* ucombineddata_;
     double* vcombineddata_;
-
-//    double* utoparraysend;
-//    double* utoparrayreceive;
-//    double* ubtmarraysend;
-//    double* ubtmarrayreceive;
-//    double* vtoparraysend;
-//    double* vtoparrayreceive;
-//    double* vbtmarraysend;
-//    double* vbtmarrayreceive;
 };
 
 
--- burgers.cpp
+++ burgers.cpp
@@ -22,117 +22,30 @@ int main(int argc, char* argv[]) {
 
     // Initialising the problem
     Model m(argc, argv); ///< Initialise Model with input command line argument as parameters
-    if(m.world_rank == 0) m.PrintParameters(); ///< Print Parameters to make sure they are correct
-    Burgers b(m); ///< Initialise Burgers Class which will contain the data with our parameters from Model
-    b.SetVelField(m); ///< Calculate the initial velocity field from initial conditions
+    if (m.world_rank == 0) m.PrintParameters(); ///< Print Parameters to make sure they are correct
+    Burgers b(m); ///< Initialise the Burgers Class which will contain the data with our parameters from Model
+    b.SetVelField(m); ///< Calculate the initial velocity field
 
 
-
-
+    // Check the time taken for the time integration
     if (m.world_rank == 0) std::cout << "Time integrating velocity field...\n";
-    // Check the time taken for calculation
     typedef std::chrono::high_resolution_clock hrc;
     typedef std::chrono::milliseconds ms;
     hrc::time_point start = hrc::now();
 
-//     Time integrate the velocity field
+    // Time integrate the velocity field
     b.TimeIntegrateVelField(m);
 
     // Print time taken to time integrate the field
     hrc::time_point end = hrc::now();
-    std::cout << "Time taken: " << std::chrono::duration_cast<ms>(end - start).count() << "ms\n";
+    std::cout << "Rank: " << m.world_rank << ", Time taken: " << std::chrono::duration_cast<ms>(end - start).count()
+              << "ms\n";
 
-
-
-//    if (m.world_rank == 0) {
-//        std::cout << " combined vel field: \n";
-//        b.DisplayCombinedufield(m);
-//
-//    }
-//
-    // velocity field printers
-//    if (m.world_rank == 0) {
-//        std::chrono::seconds dura(1);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//
-//    }
-//    if (m.world_rank == 1) {
-//        std::chrono::seconds dura(3);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 2) {
-//        std::chrono::seconds dura(4);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//
-//    if (m.world_rank == 3) {
-//        std::chrono::seconds dura(5);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 4) {
-//        std::chrono::seconds dura(6);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 5) {
-//        std::chrono::seconds dura(7);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 6) {
-//        std::chrono::seconds dura(8);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//
-//    if (m.world_rank == 7) {
-//        std::chrono::seconds dura(9);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 8) {
-//        std::chrono::seconds dura(10);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 9) {
-//        std::chrono::seconds dura(11);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 10) {
-//        std::chrono::seconds dura(12);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-//    if (m.world_rank == 11) {
-//        std::chrono::seconds dura(13);
-//        std::this_thread::sleep_for(dura);
-//        std::cout << m.world_rank << " vel field: \n";
-//        b.DisplayuVelField(m);
-//    }
-
-
-//    // Calculate final energy
-//    if (m.world_rank == 0)  std::cout << "Energy of velocity field: " << std::setprecision(5) << b.EnergyOfVelField(m) << std::endl;
+    // Display the Total Energy
+    if (m.world_rank == 0)  std::cout << "Energy of velocity field: " << std::setprecision(5) << b.EnergyOfVelField(m) << std::endl;
 
     // Print the final velocity field to VelocityFields.txt
-//    b.FilePrintVelFields(m);
+    b.FilePrintVelFields(m);
 
 
     return 0;
